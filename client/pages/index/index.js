3d65c9f2995ab9268a8d140ca8f5cbdd55e27a58
curestraw@outlook.com <curestraw@outlook.com>
2017-08-31 19:38:32 +0800

    First

---
+++ client/pages/index/index.js
@@ -0,0 +1,77 @@
+
+
+// 获取全局应用程序实例对象
+const app = getApp();
+
+// 创建页面实例对象
+Page({
+  /**
+   * 页面名称
+   */
+  name: "indexview",
+  /**
+   * 页面的初始数据
+   */
+
+  data: {
+    imgUrls: [
+      'http://ac-eh9unt0e.clouddn.com/0461f913a445a72b181f.png',
+      'http://ac-eh9unt0e.clouddn.com/781b59cb74b5521e21b2.png',
+      'http://ac-eh9unt0e.clouddn.com/f2dae0e38f276c95d110.png'
+    ],
+    indicatorDots: true,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow () {
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh () {
+    
+  },
+
+
+  //以下为自定义点击事件
+  
+  tap_0e018233:function(e){
+  },
+  
+})
+
